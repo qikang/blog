5263e2d2ee0c0016cc1971132318b1a3f8ad347a
qikang <qikang@ctfo.com>
2026-09-15 17:27:36 +0800

    fix bug

--- build-run.sh
+++ build-run.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 set -ex
 
-IMAGE_NAME="blog:latest"
-PROJECT_DIR="$(cd "$(dirname "$0")" && pwd)"
-
-docker system prune --all --force --volumes > /dev/null
-
-docker compose down
-
-cd "$PROJECT_DIR"
-
-docker build -t $IMAGE_NAME .
-
-cd "$PROJECT_DIR"
-docker compose up -d
-
-echo "服务地址: http://localhost:8083"
+go build -o blog .
+./blog
 
